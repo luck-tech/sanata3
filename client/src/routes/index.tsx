3203d32597b9132e73ecd3357b30152a1225e1d1
luck-tech <luck@imai.am>
2024-12-08 14:56:38 +0900

    app name

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -16,7 +16,7 @@ function Home() {
 
   return (
     <div className="container mx-auto flex flex-col gap-10 justify-center items-center min-h-screen">
-      <h1 className="text-5xl font-bold">サービス名</h1>
+      <h1 className="text-5xl font-bold">Haru World</h1>
       <Button
         className="text-lg [&_svg]:size-6"
         size={"lg"}
